6d891176b7ceb4e737efc55d7f0c063b96469a73
Atibrewa <atibrewa@macalester.edu>
2023-03-27 15:31:36 -0500

    adding search barrrrrrr

--- lib/main.dart
+++ lib/main.dart
@@ -200,3 +200,82 @@ class _VehicleRowState extends State<VehicleRow> {
     );
   }
 }
+
+// This creates the search bar!
+class SearchBar extends SearchDelegate {
+  // This is where the list of items (routes) need to go
+  List<String> searchTerms = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Pear",
+    "Watermelons",
+    "Blueberries",
+    "Pineapples",
+    "Strawberries"
+  ];
+
+  // This one clears the search bar of text when its clicked
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  // back arrow to exit search menu :)
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+  // shows query result? still  lil confused by this one
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var route in searchTerms) {
+      if (route.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(route);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  // this one shows query results while typing!
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var route in searchTerms) {
+      if (route.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(route);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
