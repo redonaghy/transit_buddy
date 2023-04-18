f8ca29c299d60564feea2b94f183001ed8910805
Emydius Montes <emydiusmontes@Emydiuss-MacBook-Pro.local>
2023-04-18 17:51:41 -0500

    Fixed location checks?

--- lib/main.dart
+++ lib/main.dart
@@ -39,36 +39,48 @@ class _TransitAppState extends State<TransitApp> {
   ];
   CurrentLocationLayer userLocationLayer = CurrentLocationLayer();
 
+  Future<void> completeLocationRequest() async {
+    if (await Geolocator.checkPermission() == LocationPermission.denied) {
+      await Geolocator.requestPermission();
+    }
+    startTransitStream();
+  }
+
+  // Declares the streamListener and refreshes vehicles based on first event
+  void startTransitStream() {
+    streamListener = dart_gtfs.transitStream().listen((transitFeed) {
+      setState(() {
+        debugPrint("Refreshed");
+        vehicleList = [];
+        vehicleMarkerList = [];
+        for (FeedEntity vehicle in transitFeed) {
+          if (vehicle.vehicle.trip.routeId == route &&
+              vehicle.vehicle.position.latitude != 0 &&
+              vehicle.vehicle.position.longitude != 0) {
+            vehicleList.add(vehicle);
+            vehicleMarkerList.add(Marker(
+              builder: (ctx) {
+                return VehicleMarker(angle: vehicle.vehicle.position.bearing);
+              },
+              point: LatLng(vehicle.vehicle.position.latitude,
+                  vehicle.vehicle.position.longitude),
+            ));
+          }
+        }
+        debugPrint("${vehicleMarkerList.length}");
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    completeLocationRequest().then((value) {
+      super.initState();
+    },);
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    // Declares the streamListener and refreshes vehicles based on first event
-    void startTransitStream() {
-      streamListener = dart_gtfs.transitStream().listen((transitFeed) {
-        setState(() {
-          debugPrint("Refreshed");
-          vehicleList = [];
-          vehicleMarkerList = [];
-          for (FeedEntity vehicle in transitFeed) {
-            if (vehicle.vehicle.trip.routeId == route &&
-                vehicle.vehicle.position.latitude != 0 &&
-                vehicle.vehicle.position.longitude != 0) {
-              vehicleList.add(vehicle);
-              vehicleMarkerList.add(Marker(
-                builder: (ctx) {
-                  return VehicleMarker(angle: vehicle.vehicle.position.bearing);
-                },
-                point: LatLng(vehicle.vehicle.position.latitude,
-                    vehicle.vehicle.position.longitude),
-              ));
-            }
-          }
-          debugPrint("${vehicleMarkerList.length}");
-        });
-      });
-    }
-
-
 
     // ! change to static parsed data at some point
     stopMarkerList = <Marker>[
@@ -121,7 +133,14 @@ class _TransitAppState extends State<TransitApp> {
               LatLng(45.423272, -93.961313), LatLng(44.595736, -92.668792)),
           rotationWinGestures: 90,
         ),
-        children: flutterMapLayers,
+        children: [
+          TileLayer(
+            urlTemplate: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+          MarkerLayer(markers: vehicleMarkerList + stopMarkerList),
+          CurrentLocationLayer(),
+        ],
       ),
       Container(
         alignment: Alignment.topCenter,
