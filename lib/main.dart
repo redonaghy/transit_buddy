1c94ab34c1446c489ad3a3eb9765a1aa3167d486
Emydius Montes <emydiusmontes@Emydiuss-MacBook-Pro.local>
2023-04-18 10:05:44 -0500

    Successful location implementation; now bus markers do not show up though

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 import 'package:gtfs_realtime_bindings/gtfs_realtime_bindings.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:transit_buddy/StaticData.dart';
 import 'package:transit_buddy/dart_gtfs.dart' as dart_gtfs;
 import 'package:transit_buddy/RouteSearchBar.dart';
 import 'package:transit_buddy/VehicleMarker.dart';
-import 'package:location/location.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -24,48 +26,27 @@ class _TransitAppState extends State<TransitApp> {
   StaticData staticDataFetcher = StaticData();
   List<FeedEntity> vehicleList = [];
   List<Marker> vehicleMarkerList = [];
+  List<Marker> stopMarkerList = [];
   String route = "921";
   bool initRun = true;
   late StreamSubscription<List<FeedEntity>> streamListener;
-  late LocationData _currentPosition;
-  final Location location = Location();
-  bool isLocationPresent = false;
+  late List<Widget> flutterMapLayers = [
+    TileLayer(
+      urlTemplate: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+      userAgentPackageName: 'com.example.app',
+    ),
+    MarkerLayer(markers: vehicleMarkerList + stopMarkerList),
+  ];
+  CurrentLocationLayer userLocationLayer = CurrentLocationLayer();
 
   @override
   Widget build(BuildContext context) {
-
-    // Method to pull userLocation
-    void userLocation() async {
-      bool locationServiceEnabled;
-      PermissionStatus locationPermissionGranted;
-
-      locationServiceEnabled = await location.serviceEnabled();
-      if (!locationServiceEnabled) {
-        locationServiceEnabled = await location.requestService();
-        if (!locationServiceEnabled) {
-          return;
-        }
-      }
-      locationPermissionGranted = await location.hasPermission();
-      if (locationPermissionGranted == PermissionStatus.denied) {
-        locationPermissionGranted = await location.requestPermission();
-        if (locationPermissionGranted != PermissionStatus.granted) {
-          return;
-        }
-      }
-
-      location.getLocation().then((value) {
-        _currentPosition = value;
-        isLocationPresent = true;   
-      });
-    }
 
     // Declares the streamListener and refreshes vehicles based on first event
     void startTransitStream() {
       streamListener = dart_gtfs.transitStream().listen((transitFeed) {
         setState(() {
           debugPrint("Refreshed");
-          userLocation();
           vehicleList = [];
           vehicleMarkerList = [];
           for (FeedEntity vehicle in transitFeed) {
@@ -82,30 +63,15 @@ class _TransitAppState extends State<TransitApp> {
               ));
             }
           }
-          
-          if (isLocationPresent) {
-            vehicleMarkerList.add( Marker(
-                builder: (ctx) {
-                  return Icon(Icons.my_location);
-                },
-                point: LatLng(_currentPosition.latitude!,
-                    _currentPosition.longitude!),
-            ));
-          }
+          debugPrint("${vehicleMarkerList.length}");
         });
       });
     }
 
 
-    // On first build, the app will subscribe to a stream of transit data that refreshes every 15 seconds
-    if (initRun) {
-      startTransitStream();
-      userLocation();
-      initRun = false;
-    }
 
     // ! change to static parsed data at some point
-    var stopMarkerList = <Marker>[
+    stopMarkerList = <Marker>[
       Marker(
         builder: (ctx) => Image.asset('assets/stop-bus.png'),
         point: LatLng(44.940063, -93.167231),
@@ -124,6 +90,24 @@ class _TransitAppState extends State<TransitApp> {
       ),
     ];
 
+    
+    /* On first build, the app will subscribe to a stream of transit data that 
+      refreshes every 15 seconds. We also don't add the user location layer
+      until location service status is definite so it doesn't call duplicate
+      location checks and crashes
+    */
+    if (initRun) {
+      Geolocator.isLocationServiceEnabled().then((value) {
+        debugPrint("added location layer");
+        flutterMapLayers.add(userLocationLayer);
+        setState(() {
+        });
+      });
+      startTransitStream();
+      initRun = false;
+    }
+
+
     // App interface
     return Scaffold(
         body: Stack(children: [
@@ -137,13 +121,7 @@ class _TransitAppState extends State<TransitApp> {
               LatLng(45.423272, -93.961313), LatLng(44.595736, -92.668792)),
           rotationWinGestures: 90,
         ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-          MarkerLayer(markers: vehicleMarkerList + stopMarkerList),
-        ],
+        children: flutterMapLayers,
       ),
       Container(
         alignment: Alignment.topCenter,
